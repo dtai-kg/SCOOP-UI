6c520ddbc4e1e37b58a0ae82148b1951e6b91879
Xuemin Duan <xueminduan@163.com>
2024-03-02 19:24:06 +0100

    update

--- app.py
+++ app.py
@@ -65,3 +65,5 @@ def translate():
 # if __name__ == '__main__':
 #     app.run(debug=True)
 #     # app.run(host='0:0:0:0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=6942)
