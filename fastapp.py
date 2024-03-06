9804563d6a272336bc90df968ef5fb82ff9ebdbb
Xuemin Duan <xueminduan@163.com>
2024-03-06 15:26:22 +0100

    update

--- fastapp.py
+++ fastapp.py
@@ -157,4 +157,4 @@ async def integrate(request_data: IntegrationRequest):
 
 if __name__ == "__main__":
     # uvicorn.run(app, host="193.190.127.194", port=8000)
-    uvicorn.run(app, host="0.0.0.0")
+    uvicorn.run(app, port=8000)
